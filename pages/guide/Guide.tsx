63d6bc9f6454b7c93a94492301bb140886d06b19
Jarrod <admin@jarrodli.com>
2022-11-05 00:56:13 +1100

    task(selection): breakup components to reflect paginated q/a structure

--- pages/guide/Guide.tsx
+++ pages/guide/Guide.tsx
@@ -1,4 +1,12 @@
 import React, { FunctionComponent, useState } from "react";
+import Navigator from "./components/shared/Navigator";
+import Question from "./components/shared/Question";
+import Step1 from "./components/steps/Step1";
+
+export const buttonCss =
+    "border-2 py-9 px-9 border-white hover:bg-gray-100 hover:bg-opacity-20 rounded-full bg-no-repeat bg-contain disabled:opacity-50";
+export const textCss =
+    "text-xl md:text-2xl px-8 border-2 p-4 border-white hover:bg-gray-100 hover:text-white hover:bg-opacity-20 font-inter";
 
 interface Props {}
 
@@ -14,64 +22,19 @@ const Guide: FunctionComponent<Props> = ({}) => {
         }
     };
 
-    const buttonCss =
-        "border-2 py-9 px-9 border-white hover:bg-gray-100 hover:bg-opacity-20 rounded-full bg-no-repeat bg-contain disabled:opacity-50";
-    const textCss =
-        "text-xl md:text-2xl px-8 border-2 p-4 border-white hover:bg-gray-100 hover:text-white hover:bg-opacity-20 font-inter";
-
     return (
         <div className="h-screen flex place-content-center">
             <div className="mx-8 my-48 place-content-center">
-                <p className="text-3xl md:text-4xl text-center font-bold font-inter">
-                    {" "}
-                    Select the State or Territory holding the information to be
-                    requested.{" "}
-                </p>
-                <div className="my-16 flex mx-auto place-content-center">
-                    <div className="space-x-8 space-y-8">
-                        <button
-                            onClick={() => handleSetJurisdiction("NSW")}
-                            className={`${textCss} ${
-                                jurisdiction === "NSW"
-                                    ? "bg-white text-midnights hover:bg-opacity-70"
-                                    : null
-                            }`}
-                        >
-                            New South Wales
-                        </button>
-                        <button
-                            onClick={() => handleSetJurisdiction("VIC")}
-                            className={`${textCss} ${
-                                jurisdiction === "VIC"
-                                    ? "bg-white text-midnights hover:bg-opacity-70"
-                                    : null
-                            }`}
-                        >
-                            Victoria
-                        </button>
-                        <button
-                            onClick={() => handleSetJurisdiction("QLD")}
-                            className={`${textCss} ${
-                                jurisdiction === "QLD"
-                                    ? "bg-white text-midnights hover:bg-opacity-70"
-                                    : null
-                            }`}
-                        >
-                            Queensland
-                        </button>
-                    </div>
-                </div>
-                <div className="my-24 space-x-16 flex place-content-center">
-                    <button
-                        disabled={page === 1}
-                        className={`${buttonCss} bg-arrowLeft`}
-                    />
-                    <button className={textCss}>Restart</button>
-                    <button
-                        disabled={!jurisdiction}
-                        className={`${buttonCss} bg-arrowRight`}
-                    />
-                </div>
+                <Question
+                    question={
+                        "Select the State or Territory holding the information to be requested."
+                    }
+                />
+                <Step1
+                    jurisdiction={jurisdiction}
+                    handleSetJurisdiction={handleSetJurisdiction}
+                />
+                <Navigator page={page} jurisdiction={jurisdiction} />
             </div>
         </div>
     );
